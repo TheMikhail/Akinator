67b62553d6119339c7d074986a6b57715962cb5e
TheMikhail <mdd2002@mail.ru>
2023-12-17 20:12:06 +0700

    Class Car

--- src/main/java/org/example/Car.java
+++ src/main/java/org/example/Car.java
@@ -1,16 +1,11 @@
 package org.example;
 
 public class Car {
-
+    public String name;
+    public Manufactory manufactory;
+    public String model;
+    public Market market;
     public String getName() {
         return name + " " + model;
     }
-
-    public String name;
-    public Manufactory manufactory;
-
-    public String model;
-    public Market market;
-
-
 }
