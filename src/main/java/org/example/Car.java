d74413268feba090b24befb038b3aa8bfc30e297
TheMikhail <mdd2002@mail.ru>
2023-12-17 19:52:12 +0700

    Class Car

---
+++ src/main/java/org/example/Car.java
@@ -0,0 +1,17 @@
+package org.example;
+
+public class Car {
+
+    public String name;
+    public enum Manufactory{
+        Japan,
+        American,
+        Europian
+    }
+    public String model;
+    public enum Market{
+        JDM,
+        USDM,
+        EDM
+    }
+}
